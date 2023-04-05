959c59d478e424e620174c4df7e21162b6b0f163
ugurhdml <ugur.hadimli@etiya.com>
2023-04-05 17:43:36 +0300

    Lesson Note

---
+++ Day1/LessonNotes/main.py
@@ -0,0 +1,57 @@
+print("Hello Etiya!")
+print("Welcome!")
+
+#this is a comment line !
+
+#Variables--->start
+
+#String (metinsel), numerik, obje
+
+text="Hello Etiya, Hoşgeldiniz!"
+print(text) 
+print(text)
+print(text)
+
+text="Hello!"
+print(text)
+print(text)
+
+studentcount =45 #int - integer - tamsayı
+print(studentcount+5)
+studentcount="45" #string(metinsel)
+print(studentcount)
+
+averagePoint=25.5 # double, decimal- ,float ---> ondalık sayı
+print(averagePoint+12.2)
+
+isVerified=True # bool, boolean --> returns True or False
+
+print(type(averagePoint)) #--> tip göstermek için kullanılır
+#Variables--->end
+
+
+#operators ----> start
+number = 10
+    #matematiksel operatörler 
+print(number + 10)
+print(number - 5 )
+print(number / 2)
+print(number * 5)
+print(number % 2)
+
+print(number + 5 - 2 * number / 3)
+        #mantıksal operatorler
+print(10 == 10)  # true
+print(11 == 10)  # false
+
+print(10 != 10)  # false
+print(11 != 10)  # true
+
+print(number > 10 ) # false
+print(number <= 10) # true
+
+
+
+
+#operators ----> end
+
