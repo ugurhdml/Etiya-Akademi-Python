382bbc70aa100786f9e5d8cc9ae4e9a6decd8c92
ugurhdml <ugur.hadimli@etiya.com>
2023-04-05 17:27:43 +0300

    Day1 Lesson Notes And Homeworks

---
+++ Day1/day1HomeWork.py
@@ -0,0 +1,26 @@
+baslik = "HABERİNİZ OLSUN" # -- String
+
+vade = 12 # --> integer
+faizOrani1 = 1.47 # --> float, double, decimal
+faizOrani2 = 1.44 
+
+#Değişken ismi sayi ile başlamaz.
+#Değişken isminde alt çizgi/tire harici özel karakter kullanılmaz.
+
+
+
+print(baslik)
+print(type(baslik)) # --> tip görmek için type kullanılır.
+print(type(vade))
+print(type(faizOrani1))
+
+mesaj = "Hoşgeldin"
+musteriAdi = "Uğur"
+musteriSoyadi = "Hadımlı"
+sonucMesaj=mesaj+" "+musteriAdi+" "+musteriSoyadi
+
+print(sonucMesaj)
+
+sayi1 = 15
+sayi2 = 25
+print(sayi1+sayi2)
