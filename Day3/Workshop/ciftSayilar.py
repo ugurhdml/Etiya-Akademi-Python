d614085ce267131f64985ce453f85d942cf8665b
ugurhdml <ugur.hadimli@etiya.com>
2023-04-07 14:46:13 +0300

    Homework

--- Day3/Workshop/ciftSayilar.py
+++ Day3/Workshop/ciftSayilar.py
@@ -3,8 +3,9 @@ secim = int(input("Alt limit belirlemek için 1 tuşlayınız. Belirlemeden deva
 
 if secim == 1:
     altLimit = int(input("Bir alt limit giriniz :"))
-    for i in range(altLimit,ustLimit,2):
-        print(i)
+    for i in range(altLimit,ustLimit):
+        if i % 2 ==0:
+            print(i)
 elif secim ==2:
     for i in range(0,ustLimit,2):
         print(i)
