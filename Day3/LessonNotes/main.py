84090c915cf3cae3203e062a40cc33748dc5ac12
ugurhdml <ugur.hadimli@etiya.com>
2023-04-07 14:28:00 +0300

    C:/Users/ugur.hadimli/AppData/Local/Programs/Python/Python311/python.exe

--- Day3/LessonNotes/main.py
+++ Day3/LessonNotes/main.py
@@ -1,16 +1,13 @@
-biggestValue = 0
+# Döngüler start
+for i in range (0,10):
+    print(i)
 
-#for i in range(10):
-    #sayi = int(input(f"{i+1}. Değeri Giriniz : "))
-   # if sayi > biggestValue:
-    #    biggestValue = sayi
-#print(f"Girdiğiniz sayılar arasından en büyük olanı : {biggestValue}")
+ogrenciler = ["Volkan" ,"Ahmet","Sevgi","Derin","Uğur"]
 
-sayilar = []
+for i in range(len(ogrenciler)):
+    print(f"{i+1}. Öğrenci {ogrenciler[i]}")
 
-for i in range(3):
-    sayilar.append(int(input(f"{i+1}. sayıyı giriniz : ")))
 
-sayilar.sort(reverse=True)
-enBuyukN = int(input("Sayılar arasından kaçınıcı en büyüğü istiyorsunuz?"))
-print(sayilar[enBuyukN-1])
+for ogrenci in ogrenciler:
+    pirnt(f"Ogrenci : {ogrenci}")
+# Döngüler End
