24fc28aa1ecabb1e64893c9d5c82a357f0f34e46
ugurhdml <ugur.hadimli@etiya.com>
2023-04-10 14:58:37 +0300

    10/04/23 calculator

---
+++ Day4/calculator.py
@@ -0,0 +1,51 @@
+def toplama ():
+    sayi1=int(input("Toplama işlemi için birinci sayıyı giriniz : "))
+    sayi2=int(input("Toplama işlemi için ikinci sayıyı giriniz : "))
+    print(sayi1,"+",sayi2,"=", sayi1+sayi2)
+
+def cikarma():
+    sayi1=int(input("Çıkarma işlemi için birinci sayıyı giriniz : "))
+    sayi2=int(input("Çıkarma işlemi için ikinci sayıyı giriniz : "))
+    print(sayi1,"-",sayi2,"=", sayi1-sayi2)
+
+def carpma():
+    sayi1=int(input("Çarpma işlemi için birinci sayıyı giriniz : "))
+    sayi2=int(input("Çarpma işlemi için ikinci sayıyı giriniz : "))
+    print(sayi1,"*",sayi2,"=", sayi1*sayi2)
+
+def bolme():
+    sayi1=int(input("Bölme işlemi için birinci sayıyı giriniz : "))
+    sayi2=int(input("Bölme işlemi için ikinci sayıyı giriniz : "))
+    print(sayi1,"/",sayi2,"=", sayi1/sayi2)
+
+def modAlma():
+    sayi1=int(input("Mod alma işlemi için birinci sayıyı giriniz : "))
+    sayi2=int(input("Mod alma işlemi için ikinci sayıyı giriniz : "))
+    print(sayi1,"%",sayi2,"=", sayi1%sayi2)
+
+cikisKodu = 0
+
+while cikisKodu==0:
+    secim=input("Yapmak istediğiniz işlemi giriniz: + --> Toplama  - --> Çıkarma  * --> Çarpma  / --> Bölme  % --> Mod Alma : ")
+    
+    if secim == "q":
+        print("Programdan çıkılıyor...")
+        cikisKodu=1
+
+    elif secim == '+':
+        toplama()
+        
+    elif secim == '-':
+        cikarma()
+        
+    elif secim == '*':
+        carpma()
+        
+    elif secim == '/':
+        bolme()
+
+    elif secim == "%":
+        modAlma()
+
+    else:
+        print("Geçersiz giriş yaptınız, tekrar deneyiniz...")
